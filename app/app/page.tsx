a865535b7b731afba0edae15dc5869c7e28046d3
endankiwe <danendan06@gmail.com>
2026-03-25 18:54:44 +0700

    Create page.tsx

---
+++ app/app/page.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <main style={{
+      minHeight: "100vh",
+      background: "#07090F",
+      color: "white",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column"
+    }}>
+      <h1 style={{fontSize: "50px", fontWeight: "900"}}>
+        DannStore AI 🚀
+      </h1>
+      <p style={{opacity: 0.6}}>
+        Website lu udah hidup 😭🔥
+      </p>
+    </main>
+  );
+}
